a6da90265ad52ebe12277274c7fd4dce2e74f4ec
Fouad1806 <61727632+Fouad1806@users.noreply.github.com>
2023-12-06 15:23:23 +0100

    Create dummy_classes.py
    Just for scribbles!

---
+++ python_files/dummy_classes.py
@@ -0,0 +1,33 @@
+# Ant Simulation
+
+class Ant:
+    
+    def __init__():
+        pass
+    
+    def movement():
+        pass
+    
+    def pheromone():
+        pass
+    
+    pass
+
+
+class Food_Source:
+    
+    def __init__():
+        pass
+    
+    def spawn():
+        pass
+
+    pass
+
+
+class Gui:
+    def __init__():
+        pass
+
+    pass
+
