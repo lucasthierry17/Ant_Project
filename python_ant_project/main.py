ffe9fe57bab51fed19e0db5d256c9dea6c0456fb
Sven Ochmann <sven.och@outlook.com>
2023-12-13 11:49:16 +0100

    first code structure

---
+++ python_ant_project/main.py
@@ -0,0 +1,32 @@
+
+class board():
+    """
+    board infos :
+        Position,
+        Position Colony,
+        etc
+    """
+
+
+class Ants():
+    """
+    Ant stuff :
+        Next move,
+        Pheromone,
+        Ant Antributes
+    """
+
+
+class food():
+    """
+    Food atributes :
+        smell distance,
+        can_be_smelled(),
+        Spawn food with decay,
+    """
+
+
+class view():
+    """
+    GUI
+    """
