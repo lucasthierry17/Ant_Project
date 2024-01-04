50b7d847119ad1a5382bdaaaf62bb73073f1fbaf
Lucas Mueller <lucast.mueller@googlemail.com>
2024-01-04 18:57:33 +0100

    used black as a linter

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,20 +1,21 @@
 """Tests for the function inside the time_1.py file
 """
 import pytest
-from Version1 import Ants 
+from Version1 import Ants
 from Version1 import Food
-from Version1 import Board 
+from Version1 import Board
 import numpy as np
 
 
 def test_movement():
-    num_ants = 10 # sets up the number of ants for this test function 
-    ants = Ants(num_ants) # creates an instance of the Ant class
+    num_ants = 10  # sets up the number of ants for this test function
+    ants = Ants(num_ants)  # creates an instance of the Ant class
     perception_radius = 3
     approach_speed = 0.5
-    ants.movement(perception_radius, approach_speed) # calls the movement function with the parameters perception_radius and approach_speed
+    ants.movement(
+        perception_radius, approach_speed
+    )  # calls the movement function with the parameters perception_radius and approach_speed
 
-    
     assert ants.positions.shape == (num_ants, 2)
 
 
@@ -25,25 +26,22 @@ def test_perception():
     num_food = 5
     food = Food(num_food)
 
-    # set up some values in order to check the function 
+    # set up some values in order to check the function
     ants.positions = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9]])
     food.positions = np.array([[0.5, 0.4], [1.3, 2.5], [3.3, 1.3], [5, 5.3], [8, 7]])
 
     # Set a known perception radius
     perception_radius = 1.0
 
-    # Calls the 
+    # Calls the
     perception_function = ants.perception(food, perception_radius)
-    
 
     # set up the results
     expected_results = [True, True, True, False, False, True, False, True, True, False]
-    assert perception_function == expected_results # checking if the results from the function are equal to the expected results
-
-
+    assert (
+        perception_function == expected_results
+    )  # checking if the results from the function are equal to the expected results
 
 
 def test_approach_food():
     pass
-
-
