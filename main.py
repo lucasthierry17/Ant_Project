c234a0f14b1a997cbcd809cf3ea8f52fb12a0dd8
Fouad Mokhtari <fouad.mokhtari@study.hs-duesseldorf.de>
2024-01-14 03:02:15 +0100

    Creating the map.py file

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import os
+
+
+class Board:
+    """the board class is for the visualisation of the board and for the simulation."""
+
+    def __init__(self, width, height):
+        # Defining width and height of the window
+        self.width = width
+        self.height = height
+    
+    def visualize(self, food_springs)
+    """This functio"""    
