b5b1f5f77baa9a11682e9555374c4f7f70aab758
Fouad Mokhtari <fouad.mokhtari@study.hs-duesseldorf.de>
2024-01-14 03:01:51 +0100

    Filling the init function and adding basics into the visualize function

--- map.py
+++ map.py
@@ -1,9 +1,20 @@
+import pygame
+from sys import exit
+import time
+import os
+
 class Map:
-    def __init__():
+    def __init__(self, width, height):
         """
         Hier wird die Größe der Karte festgelegt.
         """
-        pass
+        
+        self.width = width
+        self.height = height
+        pygame.init # Initialisiere Pygame
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Ants')
+        self.clock = pygame.time.Clock() # Clock Object
 
     def ant_nest():
         """
@@ -25,4 +36,21 @@ class Map:
         Die Nahrungsquellen werden ebenfalls zufällig auf der Karte platziert.
         Man kann ein int mitangeben um die Anzahl an Nahrungsquellen festzulegen.
         """
+    
+    def visualize(self):
+        '''In tthis function, all the elements will be drawn on the map.'''
+        while True:
+            for event in pygame.event.get():
+                if event.type ==  pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            # draw all elements
+            # update everything
+            pygame.display.update()
+            self.clock.tick(60) # 60fps
         pass
+
+if __name__ == '__main__':
+
+    map = Map(400,400)
+    map.visualize()
