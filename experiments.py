82e3faf2feaea006e387fcefcdf2181cbcf8f5c0
Lucas Mueller <lucast.mueller@googlemail.com>
2024-02-03 14:14:53 +0100

    Added the has_food status and also the food_pheromones

--- experiments.py
+++ experiments.py
@@ -14,7 +14,7 @@ nest_position = np.array([width // 2, height // 2])  # set nest to the middle
 home_pheromone = 255  # value of intensity added to the position
 food_pheromone = 255  # value of intensity added to the position
 step_size = 4  # step_size of the ants
-decay_rate = 3  # value is subtracted from the intensity in position x, y
+decay_rate = 4  # value is subtracted from the intensity in position x, y
 num_food = 200  # number of food items in the food source
 radius = 7  # radius of the food source
 size_food = 5  # size of each food item
@@ -24,12 +24,12 @@ class Ants:
         self.has_food = np.zeros((num_ants), dtype=bool)
 
     def calculate_direction(self, start, target):
-        """Calculates the direction from the start to the target. It takes in the position of the target and the position of the nest."""
+        """Calculates the direction from the start to the target."""
         direction_vector = target - start
         return direction_vector / np.linalg.norm(direction_vector)
 
     def move_ants(self, positions, directions):
-        """Represents the movement of the ants. Each ant receives a random direction for the beginning. After their first step, they have a side of view which is limited to the pheri variable. They get a random direction and take a step in that direction. If their next step is outside the screen, they take a step towards the center."""
+        """Represents the movement of the ants."""
         cos_directions = step_size * np.cos(np.radians(directions))
         sin_directions = step_size * np.sin(np.radians(directions))
 
@@ -54,7 +54,6 @@ class Ants:
 
 
 class Food:
-    """In this class, the food_sources are being created. They are shown as x of smaller green circles inside of a bigger circle. They are being placed randomly inside a certain area (away from the nest)"""
     def __init__(self, num_food, radius, min_distance=400):
         self.num_food = num_food
         self.radius = radius
@@ -88,7 +87,6 @@ class Drawing:
         self.food_positions = Food(num_food, radius).generate_positions()
 
     def draw_pheromones(self, surface, home_pheromones, food_pheromones):
-        """This function draws the pheromones. It draws small rectangles on the screen, the values for the color are being stored inside two numpy arrays (food_pheromones and home_pheromones). The pheromones disappear through the substraction of the decay rate each step"""
         for (x, y), intensity in np.ndenumerate(home_pheromones):
             home_pheromones[x, y] = max(intensity - decay_rate, 0)
         for (x, y), intensity in np.ndenumerate(food_pheromones):
@@ -98,13 +96,11 @@ class Drawing:
         
 
     def draw_food(self, size_food):
-        """This function draws the food_sources at the generated location"""
         # this function takes in the size of the food items and draws the food source on the screen
         for food_pos in self.food_positions:
             pygame.draw.circle(self.screen, (0, 255, 0), food_pos.astype(int), size_food)
 
     def draw_ants(self, ant_positions, size):
-        """The ants are drawn in red with the size specified in the size_ant variable. Also, we are calling the other drawing functions, so that the elements are drawn on to the screen as well"""
         # this function takes in the screen, the position and size of the ant and draws them in
         self.screen.fill((0, 0, 0))
         self.draw_pheromones(self.surface, main.home_pheromones, main.food_pheromones)
