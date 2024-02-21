2d1ff03b0cabc7b209445c2fc08adfa1094aaa2c
Nick Grabowski <na.grabowski@gmail.com>
2024-02-21 20:29:54 +0100

    seperate input for speed and number of ants

--- start_screen.py
+++ start_screen.py
@@ -1,6 +1,8 @@
 import pygame
 
 
+clock = pygame.time.Clock()
+
 class StartMenu:
     def __init__(self) -> None:
         self.screen_width = 750
@@ -10,12 +12,17 @@ class StartMenu:
         self.speed = ''
         self.initialize_start_menu()
         self.game_state = "start_menu"
-        self.cursor_visible = True
-        self.cursor_blink_interval = 500  # in milliseconds
-        self.cursor_blink_timer = 0
+        # Cursor variables
+        self.ants_cursor = 0
+        self.ants_box_active = False
+        self.speed_cursor = 0 
+        self.speed_box_active = False
+
+
 
 
     def initialize_start_menu(self):
+        white_text =(255, 255, 255)
          # initialize font and size (for header and Start button)
         self.font = pygame.font.SysFont('Impact', 40)
         self.title = self.font.render('Ant Search Simulation', True, (0, 255, 0)) # title in green
@@ -29,17 +36,7 @@ class StartMenu:
 
         # font for the text in the input box
         self.textbox_font = pygame.font.SysFont('Arial', min(25, int(0.8 * self.ants_text.get_height())))
-        
-    
-    def draw_cursor(self, box_rect, text_surface):
-        if pygame.time.get_ticks() - self.cursor_blink_timer > self.cursor_blink_interval:
-            self.cursor_visible = not self.cursor_visible
-            self.cursor_blink_timer = pygame.time.get_ticks()
 
-        if self.cursor_visible:
-            cursor_x = box_rect.x + text_surface.get_width()
-            cursor_y = box_rect.y + (box_rect.height - text_surface.get_height()) // 2
-            pygame.draw.line(self.screen, (255, 255, 255), (cursor_x, cursor_y), (cursor_x, cursor_y + text_surface.get_height()), 2)
 
 
     def draw(self):
@@ -61,17 +58,43 @@ class StartMenu:
         self.screen.blit(self.speed_text,(self.screen_width/2 - self.speed_text.get_width()/2, self.screen_height - 230))
 
         white_text = (255,255,255)
-       
+    
         # input box for the user to type in the number of ants
         self.ants_box = pygame.Rect(self.screen_width / 2 - self.ants_text.get_width() / 2, self.screen_height - 270, self.ants_text.get_width(), self.ants_text.get_height())
+
         # input box for the speed of the ants
         self.speed_box = pygame.Rect(self.screen_width / 2 - self.ants_text.get_width() / 2, self.screen_height - 200, self.ants_text.get_width(), self.ants_text.get_height())
 
-        pygame.draw.rect(self.screen, white_text, self.ants_box, 2)
-        self.text_surface_ants = self.textbox_font.render(self.num_ants, True, white_text)
-        width = max(200, self.text_surface_ants.get_width()+10)
-        self.ants_box.w = width
+        pygame.draw.rect(self.screen, white_text, self.ants_box, 2)        
+
+        if self.ants_box_active:
+            # text cursor 
+            if self.ants_cursor % 4 == 0:
+                self.num_ants += "|"
+            self.ants_cursor += 1 
+            try:
+                if self.num_ants[-1] == "|":
+                    self.num_ants = self.num_ants[:-1]
+            except:
+                pass
         
+        if self.speed_box_active:
+            # cursor
+            if self.speed_cursor % 4 == 0:
+                self.speed += "|"
+            self.speed_cursor +=1
+            try:
+                if self.speed[-1] == "|":
+                    self.speed = self.speed[:-1]
+            except:
+                pass
+
+            
+
+        text_surface_ants = self.textbox_font.render(self.num_ants, True, white_text)
+
+        width = max(200, text_surface_ants.get_width()+10)
+        self.ants_box.w = width
 
 
         pygame.draw.rect(self.screen, white_text, self.speed_box, 2)
@@ -80,12 +103,17 @@ class StartMenu:
         self.speed_box.w = width
 
         # Center the text horizontally within the box
-        text_x = self.ants_box.x + ((self.ants_box.width - text_surface_speed.get_width()) // 2) - 30
-        text_y = self.ants_box.y + (self.ants_box.height - text_surface_speed.get_height()) // 2
-        self.screen.blit(self.text_surface_ants, (text_x, text_y))
-        self.screen.blit(text_surface_speed,(text_x, text_y))
+        ants_text_x = self.ants_box.x + ((self.ants_box.width - text_surface_ants.get_width()) // 2) - 20
+        ants_text_y = self.ants_box.y + (self.ants_box.height - text_surface_ants.get_height()) // 2
+        
+        speed_text_x = self.ants_box.x + ((self.speed_box.width - text_surface_speed.get_width()) // 2) - 20
+        speed_text_y = self.speed_box.y + (self.speed_box.height - text_surface_speed.get_height()) // 2
+
+        self.screen.blit(text_surface_ants, (ants_text_x, ants_text_y))
+        self.screen.blit(text_surface_speed,(speed_text_x, speed_text_y))
 
         pygame.display.update()
+
 
     def handle_events(self):
             for event in pygame.event.get():
@@ -93,7 +121,7 @@ class StartMenu:
                     pygame.quit()
                     quit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.type == pygame.K_RETURN:
+                    if event.key == pygame.K_RETURN:
                         try:
                             # Convert the input_text to an integer
                             self.num_ants = int(self.num_ants)
@@ -101,20 +129,38 @@ class StartMenu:
                         except ValueError:
                             print("Please enter a valid number.")
                         self.num_ants = ''
-                    elif event.key == pygame.K_BACKSPACE:
-                        self.num_ants = self.num_ants[:-1]
-                    else:
+
+                    if self.ants_box_active: 
                         self.num_ants += event.unicode
+                        if event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE:
+                            self.num_ants = self.num_ants[:-1]
+
+                    elif self.speed_box_active:
+                        self.speed += event.unicode
+                        if event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE:
+                            self.speed = self.speed[:-1]
+
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 
+                    # activate input for number of ants
                     if self.ants_box.collidepoint(event.pos):
-                        self.draw_cursor(self.ants_box, self.text_surface_ants)
+                        self.ants_box_active = True
+                    else:
+                        self.ants_box_active = False
+
+                    # activate input for speed value
+                    if self.speed_box.collidepoint(event.pos):
+                        self.speed_box_active = True
+                    else:
+                        self.speed_box_active = False
 
                     if self.start_button_rect.collidepoint(event.pos):
                         print(f"Number of Ants: {int(self.num_ants)}")
                         print(f"Speed: {int(self.speed)}")
                         self.game_state = "Simulation"
+            clock.tick(60)   
 
+   
 
 def main():
     pygame.init()
@@ -122,7 +168,7 @@ def main():
 
     while True:
         start_menu.handle_events()
-        start_menu.draw()    
+        start_menu.draw()
 
 if __name__ == "__main__":
     main()   
