4862fd05c1fdf4b8607c8b42391b5e80bca56ce5
Fouad Mokhtari <fouad.mokhtari@study.hs-duesseldorf.de>
2024-02-22 20:50:56 +0100

    Modifying the ant movement

--- Phero.py
+++ Phero.py
@@ -4,15 +4,18 @@ import math
 import random
 
 WIDTH, HEIGHT = 1200, 800
-num_ants = 10
-PRATIO = 4 # ratio between screen and phero_grid
+max_distance = 0
+num_ants = 200
+PRATIO = 5 # ratio between screen and phero_grid
 nest = (WIDTH // 3.5, HEIGHT // 2)
 VSYNC = True
 SHOWFPS = True
 food_sources = []
-ALPHA = 7
-BETA = 1
-PHERO_THRESH = 5
+ALPHA = 5
+BETA = 0.5
+PHERO_THRESH = 70
+EVAPURATION_RATE = 0.8
+STEPSIZE = 2
 ant_positions_home = np.zeros((num_ants + 1, 2), dtype=int)
 ant_positions_home[-1] = nest
 amnt_food = len(food_sources)
@@ -35,6 +38,7 @@ class Ants(pygame.sprite.Sprite):
         self.search_for_food = False
         self.alpha = ALPHA
         self.beta = BETA
+        self.coming_from_food_source = None
         self.backend_pheromones_home = np.full((num_ants+1), 1)
         self.backend_pheromones_home[-1] = 100
         self.backend_pheromones_food = np.full((num_ants+len(food_sources)),0)
@@ -42,138 +46,181 @@ class Ants(pygame.sprite.Sprite):
         
    
 
-    def get_pheromone_positions(self):
-        '''
-        Getting the positions on the board where the Pheromon Value is greater than the Threshold.
-        Also getting the actual pheromone value at those Positions
-        '''
-        # Assuming PHERO_THRESH is defined somewhere in your code
-        high_phero = np.argwhere(self.phero.img_array[:, :, 1] > PHERO_THRESH)
+    def get_pheromone_positions(self, color='green'):
 
-        print(f"High Phero:\n{high_phero}")
-        phero_positions = np.array(list(zip(high_phero[:, 0], high_phero[:, 1])))
-        print(f"Phero Positions:\n{phero_positions}")
+        # Getting all the positions that are greater than the Pheromone Threshold
+        if color == 'green':
+            high_phero_positions = np.argwhere(self.phero.img_array[:, :, 1] > PHERO_THRESH)
+        elif color == 'blue':
+            high_phero_positions = np.argwhere(self.phero.img_array[:, :, 2] > PHERO_THRESH)
 
-        high_phero_differences = phero_positions - self.rect.center
-        high_phero_distances = np.linalg.norm(high_phero_differences, axis=1)
-        print(f"Phero Distances:\n{high_phero_distances}")
-
-        # Assuming food_sources is defined somewhere in your code
-        # You might need to adjust the length based on your actual requirements
-        phero_values = high_phero_distances / 10
-        #print(f"Unrounded Distances:\n{1000 / high_phero_distances}")
-
-        # Print or use phero_values as needed
-        print(f"Pheromone Values:\n {phero_values}")
-
-        return phero_positions, phero_values
-
-
-    def update(self):
-        scaled_pos = (int(self.x / PRATIO), int(self.y / PRATIO))
-        # Move the ant
+        # Getting the actual values on those positions 
+        phero_values = self.phero.img_array[high_phero_positions[:, 0], high_phero_positions[:, 1], 1]
         
+        return high_phero_positions, phero_values
+    
+    
+    def get_nearest_food_source(self):
+        nearest_food_source = None
+        min_distance_food = float('inf')  # Setze min_distance_food auf unendlich zu Beginn
 
+        # If the ant has no food origin it should follow the nearest food source
+    
+        for food_source in food_sources:
+            food_source = np.array(food_source) * PRATIO
+            
+            distance_food = np.linalg.norm(food_source - self.rect.center)
+
+            if distance_food < min_distance_food:
+                min_distance_food = distance_food
+                nearest_food_source = food_source
+    
+        return nearest_food_source
+
+
+
+    def colony_optimization(self, positions, pheromones, alpha=ALPHA, beta=BETA, divident=3):
+
+        # Getting Differences between the positions and the ant itself
+        differences = positions - self.rect.center
+        #print(f"Positions:\n{positions}")
+        #print(f"Nest:\n{nest}")
+        #print(f"differences:\n{differences}")
+        # Calculation of eta, which contains the actual distance to those positions
+        eta = np.linalg.norm(differences, axis=1)
+        #print(f"eta:\n{eta}")
+        #print(f"pheromones:\n{pheromones}")
+        # Total forr the nominator
+        total = sum((pheromones ** alpha) * (eta ** beta))
+
+        # Calculating the probabilities
+        probabilities = ((pheromones ** alpha) * (eta ** beta)) / total
+        #print(f"probabilities:\n{probabilities}")
+        cumulative_probs = np.cumsum(probabilities)
+        random_value = np.random.rand()
+        selected_position_index = np.searchsorted(cumulative_probs, random_value)
+        direction_vector = differences[selected_position_index] / (eta[selected_position_index] / divident)
+        #print(f"Probs:\n{probabilities}")
+        #print(f"selected_direction_vector: {differences[selected_position_index]}\nThe Position which was given:{positions[selected_position_index]}")
+        return direction_vector
+
+    
+    def update(self):
+        global max_distance
+        scaled_pos = (int(self.x / PRATIO), int(self.y / PRATIO))
+
+        # Move the ant
         if self.has_food: # ant has food
             distance = self.calculate_distance(scaled_pos, ((nest[0] / PRATIO), (nest[1] / PRATIO)))
+            if distance > max_distance:
+                max_distance = distance
+
             if distance < 5: # ant has reached the nest
                 self.has_food = False 
-                """
-                elif distance > 30 or self.phero.img_array[scaled_pos][2] < 75:
-                    self.desireDir = pygame.Vector2(nest[0] - self.x, nest[1] - self.y).normalize() # go towards the nest
-                    
-                    random_angle = random.uniform(-50, 50)
-                    self.desireDir.rotate_ip(random_angle)
-                    # Blend the straight direction with the random direction
+             
+            elif self.phero.img_array[scaled_pos[0] % self.phero.img_array.shape[0], scaled_pos[1] % self.phero.img_array.shape[1]][2] > 10 or self.has_food:
                 
-                else:
-                    self.desireDir = pygame.Vector2(nest[0] - self.x, nest[1] - self.y).normalize() # go towards the nest
-                """
-            else:
-                
-                # Calculation of all differences from the ants current position to the others
-                differences = ant_positions_home - self.rect.center
-                #print(f"Alle Richtungsvektoren:\n{differences}")
-                
-                # Calculation of the actual distanc
-                eta = np.linalg.norm(differences, axis=1)
+                pheromone_position, pheromone_values = self.get_pheromone_positions(color='blue')
+                pheromone_position = pheromone_position * PRATIO
 
-                # Total for the nominator:
-                total = sum((self.backend_pheromones_home ** self.alpha) * (eta ** self.beta))
+                # Sobald die Distanz zum Pheromon groß genug ist wird self.searcg_for_food auf False gesetztt:
+                min_distance_phero = 0
+                distances_phero = pheromone_position - self.rect.center
+                distances_phero = np.linalg.norm(distances_phero, axis=0)
 
-                # Calculating the Probabilities:
-                probs = ((self.backend_pheromones_home ** ALPHA) * (eta ** BETA)) / total
-                #print(f"Wahrscheinlichkeiten: {probs}")
-                # Choosing the next direction according to the calculated probs
-                cumulative_probs = np.cumsum(probs)
-                random_value = np.random.rand() # Random Value between 0 and 1
-                selected_position_index = np.searchsorted(cumulative_probs, random_value)
-                #print(f"Ausgesuchte Position: {selected_position_index}")
-                # Richtungsvektor wird ausgesucht und durch den eigenen halbierten Betrag des Richtungsvektors dividiert
-                direction_vector = differences[selected_position_index] / (eta[selected_position_index] / 3)
+                indices =  np.where(distances_phero < 10)
+                pheromone_position = pheromone_position[indices]
+                pheromone_values = pheromone_values[indices]
+                #print(f"Pheromon Werte bevorr nest:\n{pheromone_values}")
+
+                pheromone_position = np.append(pheromone_position, [np.array(nest)], axis=0)
+                pheromone_values = np.append(pheromone_values, 1000)
+                direction_vector = self.colony_optimization(pheromone_position, pheromone_values)
                 
-                # Checking for negative Values:
-
-                #print(f"Direction Vector: {direction_vector}")
-                #print(f"Der Punkt zuvor: ({self.x}, {self.y})")
                 self.x += direction_vector[0]
                 self.y += direction_vector[1]
-                #print(f"Der neue Punkt: ({self.x}, {self.y})")
-                self.phero.img_array[scaled_pos] += (0, 100, 0) # update pheromones
+                
+                #self.phero.img_array[scaled_pos] += (0, 100, 0) # update pheromones
+            pheromone_value = 255 * (distance / max_distance)
+            if pheromone_value > 255:
+                pheromone_value = 255  
+            self.phero.img_array[scaled_pos] += (0, pheromone_value, 0)
 
 
         else: # ant has no food
+            
             if food_sources:
                 for i,food in enumerate(food_sources):
                     distance = self.calculate_distance(scaled_pos, food)
                     
-                    if distance < 9: # reaches the food source
+                    if distance < 7: # reaches the food source
                         self.has_food = True
+                        self.coming_from_food_source = self.get_nearest_food_source()
+                        food = self.coming_from_food_source / PRATIO
+                        
+                        self.search_for_food = False
                         break
             
-                    
-                    elif self.phero.img_array[scaled_pos][1] > 20: # smells and goes to the food
+
+                    elif self.phero.img_array[scaled_pos[0] % self.phero.img_array.shape[0], scaled_pos[1] % self.phero.img_array.shape[1]][1] > 10 or self.search_for_food: # smells and goes to the food
+ 
                         #self.desireDir = pygame.Vector2(food[0] - scaled_pos[0], food[1] - scaled_pos[1])
                         food_index = i + amnt_food
-                        
+                        self.search_for_food = True
+
                         pheromone_position, pheromone_values = self.get_pheromone_positions()
-                        pheromone_position[food_index] = np.array(food) * PRATIO
-                        distance_ant_food = np.linalg.norm(self.rect.center - (np.array(food)) * PRATIO)
-                        print(f"Distance_ant_food:\n{distance_ant_food}")
+                        pheromone_position = pheromone_position * PRATIO
 
-                        if distance_ant_food < 50:
-                            pheromone_values[food_index] = 0
-                        else:
-                            pheromone_values[food_index] = 100000000 / distance_ant_food
-                        differences = pheromone_position - self.rect.center
-                        # Calculation of the actual distanc
-                        eta = np.linalg.norm(differences, axis=1)
-                        print(f"eta: {eta}\n{eta.shape}")
-                        #print(f"Eta:\n{eta}\n{eta.shape}")
-                        #print(f"Eta:\n{eta}")
-                        print(f"Pheromone_values:{pheromone_values}\n{pheromone_values.shape}")
+                        # Sobald die Distanz zum Pheromon groß genug ist wird self.searcg_for_food auf False gesetztt:
+                        min_distance_phero = 0
+                        distances_phero = pheromone_position - self.rect.center
+                        distances_phero = np.linalg.norm(distances_phero, axis=1)
 
-                        # Total for the nominator:
-                        total = sum((pheromone_values ** self.alpha) * (eta ** self.beta))
+                        indices =  np.where(distances_phero < 100)
+                        pheromone_position = pheromone_position[indices]
+                        pheromone_values = pheromone_values[indices]
+                        #print(f"Distances_phero: {distances_phero}")
 
-                        # Calculating the Probabilities:
-                        probs = ((pheromone_values ** ALPHA) * (eta ** BETA)) / total
-                        #print(f"Wahrscheinlichkeiten: {probs}")
-                        #print(f"Wahrscheinlichkeiten: {probs}")
-                        # Choosing the next direction according to the calculated probs
-                        cumulative_probs = np.cumsum(probs)
-                        random_value = np.random.rand() # Random Value between 0 and 1
-                        selected_position_index = np.searchsorted(cumulative_probs, random_value)
-                        #print(f"Ausgesuchte Position: {selected_position_index}")
-                        # Richtungsvektor wird ausgesucht und durch den eigenen halbierten Betrag des Richtungsvektors dividiert
-                        direction_vector = differences[selected_position_index] / (eta[selected_position_index] / 2)
+                        min_distance_phero = np.min(distances_phero)
+                        if min_distance_phero > 10:
+                            self.search_for_food = False
+                            self.coming_from_food_source = None
+
+                        # Looking for the nearest food_source:
                         
-                        # Checking for negative Values:
+                        #print(f"self.coming_from_food_source: {self.coming_from_food_source}")
+                        nearest_food_source = None                
+                        # If the ant has no food origin it should follow the nearest food source
+                        if not np.any(self.coming_from_food_source):
+                            nearest_food_source = self.get_nearest_food_source()
+                            pheromone_position = np.append(pheromone_position, [np.array(nearest_food_source)], axis=0)
+                            #print(f"Pheromone_position:\n{pheromone_position}\n food_position: {nearest_food_source}")
+                            pheromone_values = np.append(pheromone_values, 10000)
+                            #print(f"Pheromone_value:\n{pheromone_values}"
+                            direction_vector = self.colony_optimization(positions=pheromone_position, pheromones=pheromone_values, divident=1)
 
+                        elif np.any(self.coming_from_food_source):
+                            pheromone_position = np.append(pheromone_position, [np.array(self.coming_from_food_source)], axis=0)
+                            #print(f"Pheromone_position:\n{pheromone_position}\n food_position: {nearest_food_source}")
+                            pheromone_values = np.append(pheromone_values, 10000)
+                            #print(f"Pheromone_value:\n{pheromone_values}"
+                            direction_vector = self.colony_optimization(positions=pheromone_position, pheromones=pheromone_values, divident=1)
+
+                        else:
+                            pheromone_position = np.append(pheromone_position, [np.array(nearest_food_source)], axis=0)
+                            #print(f"Pheromone_position:\n{pheromone_position}\n food_position: {nearest_food_source}")
+                            pheromone_values = np.append(pheromone_values, 10000)
+                            #print(f"Pheromone_value:\n{pheromone_values}"
+                            direction_vector = self.colony_optimization(positions=pheromone_position, pheromones=pheromone_values, divident=1)
+                        
                         
                         self.x += direction_vector[0]
                         self.y += direction_vector[1]
-
+                    else:
+                        angle_change = random.uniform(*self.angle_range)
+                        self.desireDir = self.desireDir.rotate(angle_change).normalize()
+                        #print(f"food_array: {food_array}\nhome_array: {home_array}")
+                        # Update pheromones
 
             # Move randomly if no food source is found
             
@@ -183,15 +230,16 @@ class Ants(pygame.sprite.Sprite):
             # Update pheromones
             self.phero.img_array[scaled_pos] += (0, 0, 50)
         
-        self.x += self.desireDir[0] * 2
-        self.y += self.desireDir[1] * 2
-        
+        self.x += self.desireDir[0] * STEPSIZE
+        self.y += self.desireDir[1] * STEPSIZE
+        if not food_sources:
+            self.search_for_food = False
         # Check for collisions with screen boundaries
         if not pygame.Rect(0, 0, WIDTH, HEIGHT).collidepoint(self.x, self.y):
             # Bounce back if the ant goes out of the screen 
             self.desireDir *= -1
-            self.x += self.desireDir[0] * 2
-            self.y += self.desireDir[1] * 2
+            self.x += self.desireDir[0] * STEPSIZE
+            self.y += self.desireDir[1] * STEPSIZE
 
         self.rect.center = (self.x, self.y)
 
@@ -210,7 +258,7 @@ class Pheromones:
         
 
     def update(self):
-        self.img_array -= 0.5  # Evaporation rate
+        self.img_array -= EVAPURATION_RATE 
         self.img_array = self.img_array.clip(0, 255) # clip to color range
         pygame.surfarray.blit_array(self.image, self.img_array) 
         return self.image
